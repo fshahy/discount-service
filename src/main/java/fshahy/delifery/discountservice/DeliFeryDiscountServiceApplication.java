7b85d1c4ede031a2f378055d37bb4a4c7d48a0a6
Farid Shahy <fshahy@gmail.com>
2020-11-12 18:21:32 +0330

    init maven project.

---
+++ src/main/java/fshahy/delifery/discountservice/DeliFeryDiscountServiceApplication.java
@@ -0,0 +1,13 @@
+package fshahy.delifery.discountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliFeryDiscountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliFeryDiscountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fshahy/delifery/discountservice/DeliFeryDiscountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fshahy.delifery.discountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliFeryDiscountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
